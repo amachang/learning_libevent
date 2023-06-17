e4f49b2b43bc22221eaef925a1aed80d7858f33a
amachang <seijro@gmail.com>
2023-06-18 05:00:30 +0900

    check conan version must be 1.xx.x

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::{
+    process::exit,
     env,
     path::{
         Path,
@@ -6,8 +7,9 @@ use std::{
     },
 };
 
-use conan::*;
+use conan;
 use bindgen;
+use regex::Regex;
 
 fn main() {
     println!("cargo:rerun-if-changed=conanfile.txt");
@@ -15,15 +17,37 @@ fn main() {
 
     let conan_profile = "default";
 
-    let command = InstallCommandBuilder::new()
+    if None == conan::find_program() {
+        eprintln!("Conan command not found!");
+        exit(1);
+    };
+
+    let conan_version_regex = Regex::new(r"^(\d+)\.(\d+).(\d+)$").unwrap();
+    let conan_version = conan::find_version();
+    let Some(conan_version) = conan_version else {
+        eprintln!("Unrecognized conan version!");
+        exit(1);
+    };
+    let conan_version_captures = conan_version_regex.captures(&conan_version).expect("Invalid version number.");
+    let conan_major_version = conan_version_captures[1].parse::<u8>();
+    let Ok(conan_major_version) = conan_major_version else {
+        eprintln!("Unrecognized conan version!: {}", conan_version);
+        exit(1);
+    };
+    if conan_major_version != 1 {
+        eprintln!("Unsupported conan version!: {}", conan_version);
+        exit(1);
+    };
+
+    let command = conan::InstallCommandBuilder::new()
         .with_profile(&conan_profile)
-        .build_policy(BuildPolicy::Missing)
+        .build_policy(conan::BuildPolicy::Missing)
         .recipe_path(Path::new("conanfile.txt"))
         .build();
 
     let Some(build_info) = command.generate() else {
         eprintln!("Conan command failed!: args={:?} output_dir={:?} output_file={:?}", command.args(), command.output_dir(), command.output_file());
-        std::process::exit(1);
+        exit(1);
     };
 
     build_info.cargo_emit();
