d6e6e23b3f29ebe0a78743ef988b3ce1676f59e8
amachang <seijro@gmail.com>
2023-06-18 01:49:03 +0900

    fix indent

--- src/main.c
+++ src/main.c
@@ -12,78 +12,78 @@
 static const unsigned short PORT = 9995;
 
 static void listener_cb(struct evconnlistener *, evutil_socket_t,
-    struct sockaddr *, int socklen, void *);
+        struct sockaddr *, int socklen, void *);
 static void conn_writecb(struct bufferevent *, void *);
 static void conn_readcb(struct bufferevent *, void *);
 static void conn_eventcb(struct bufferevent *, short, void *);
 static void signal_cb(evutil_socket_t, short, void *);
 
-int
+    int
 main(int argc, char **argv)
 {
-	struct event_base *base = event_base_new();
-	if (!base) {
-		fprintf(stderr, "Could not initialize libevent!\n");
-		return 1;
-	}
+    struct event_base *base = event_base_new();
+    if (!base) {
+        fprintf(stderr, "Could not initialize libevent!\n");
+        return 1;
+    }
 
-	struct sockaddr_in sin = {0};
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(PORT);
+    struct sockaddr_in sin = {0};
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(PORT);
 
-	struct evconnlistener *listener = evconnlistener_new_bind(base, listener_cb, (void *)base,
-	    LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
-	    (struct sockaddr*)&sin,
-	    sizeof(sin));
+    struct evconnlistener *listener = evconnlistener_new_bind(base, listener_cb, (void *)base,
+            LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
+            (struct sockaddr*)&sin,
+            sizeof(sin));
 
-	if (!listener) {
-		fprintf(stderr, "Could not create a listener!\n");
-		return 1;
-	}
+    if (!listener) {
+        fprintf(stderr, "Could not create a listener!\n");
+        return 1;
+    }
 
-	struct event *signal_event = evsignal_new(base, SIGINT, signal_cb, (void *)base);
+    struct event *signal_event = evsignal_new(base, SIGINT, signal_cb, (void *)base);
 
-	if (!signal_event || event_add(signal_event, NULL)<0) {
-		fprintf(stderr, "Could not create/add a signal event!\n");
-		return 1;
-	}
+    if (!signal_event || event_add(signal_event, NULL)<0) {
+        fprintf(stderr, "Could not create/add a signal event!\n");
+        return 1;
+    }
 
     printf("Start listening the port: %d\n", PORT);
-	event_base_dispatch(base);
+    event_base_dispatch(base);
 
-	evconnlistener_free(listener);
-	event_free(signal_event);
-	event_base_free(base);
+    evconnlistener_free(listener);
+    event_free(signal_event);
+    event_base_free(base);
 
-	printf("done\n");
-	return 0;
+    printf("done\n");
+    return 0;
 }
 
-static void
+    static void
 listener_cb(struct evconnlistener *listener, evutil_socket_t fd,
-    struct sockaddr *sa, int socklen, void *user_data)
+        struct sockaddr *sa, int socklen, void *user_data)
 {
-	struct event_base *base = user_data;
-	struct bufferevent *bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
-	if (!bev) {
-		fprintf(stderr, "Error constructing bufferevent!");
-		event_base_loopbreak(base);
-		return;
-	}
-	bufferevent_setcb(bev, conn_readcb, conn_writecb, conn_eventcb, NULL);
-	bufferevent_enable(bev, EV_WRITE | EV_READ);
+    struct event_base *base = user_data;
+    struct bufferevent *bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+    if (!bev) {
+        fprintf(stderr, "Error constructing bufferevent!");
+        event_base_loopbreak(base);
+        return;
+    }
+    bufferevent_setcb(bev, conn_readcb, conn_writecb, conn_eventcb, NULL);
+    bufferevent_enable(bev, EV_WRITE | EV_READ);
 }
 
-static void
+    static void
 conn_writecb(struct bufferevent *bev, void *user_data)
 {
-	struct evbuffer *output = bufferevent_get_output(bev);
-	if (evbuffer_get_length(output) == 0) {
-		printf("Answered\n");
-	}
+    struct evbuffer *output = bufferevent_get_output(bev);
+    if (evbuffer_get_length(output) == 0) {
+        printf("Answered\n");
+    }
 }
 
-static void
+    static void
 conn_readcb(struct bufferevent *bev, void *user_data)
 {
     struct evbuffer* evbuf_in = bufferevent_get_input(bev);
@@ -99,7 +99,7 @@ conn_readcb(struct bufferevent *bev, void *user_data)
     printf("Received\n");
 }
 
-static void
+    static void
 conn_eventcb(struct bufferevent *bev, short events, void *user_data)
 {
 
@@ -111,36 +111,36 @@ conn_eventcb(struct bufferevent *bev, short events, void *user_data)
         printf("Error encountered while writing: %s\n", strerror(errno));
     }
 
-	if (events & BEV_EVENT_EOF) {
-		printf("Eof reached.\n");
-	}
-    
+    if (events & BEV_EVENT_EOF) {
+        printf("Eof reached.\n");
+    }
+
     if (events & BEV_EVENT_ERROR) {
-		printf("Unrecoverable error encountered: %s\n", strerror(errno));
-	}
+        printf("Unrecoverable error encountered: %s\n", strerror(errno));
+    }
 
     if (events & BEV_EVENT_TIMEOUT) {
         printf("User-specified timeout reached.\n");
     }
 
     if (events & BEV_EVENT_CONNECTED) {
- 	    printf("Connect operation finished.\n");
+        printf("Connect operation finished.\n");
     }
- 
- 
-	/* None of the other events can happen here, since we haven't enabled
-	 * timeouts */
-	bufferevent_free(bev);
+
+
+    /* None of the other events can happen here, since we haven't enabled
+     * timeouts */
+    bufferevent_free(bev);
 }
 
-static void
+    static void
 signal_cb(evutil_socket_t sig, short events, void *user_data)
 {
-	struct event_base *base = user_data;
-	struct timeval delay = { 2, 0 };
+    struct event_base *base = user_data;
+    struct timeval delay = { 2, 0 };
 
-	printf("Caught an interrupt signal; exiting cleanly in two seconds.\n");
+    printf("Caught an interrupt signal; exiting cleanly in two seconds.\n");
 
-	event_base_loopexit(base, &delay);
+    event_base_loopexit(base, &delay);
 }
 
